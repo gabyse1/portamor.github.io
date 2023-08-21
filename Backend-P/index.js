0168387357825224b5da6879f92bd11727b30e96
Gaby <gaby.es.sd@gmail.com>
2023-08-21 17:34:11 -0500

    Deploy commit.

--- Backend-P/index.js
+++ Backend-P/index.js
@@ -3,6 +3,7 @@ const serverApp   = require('./src/app');
 
 require('dotenv').config();
 
+// starting API
 conn.sync({ alter: false }).then(()=> {
   serverApp.listen(process.env.PORT, () => {
     console.log( "\n" + "%s listening at " + process.env.PORT + "\n" + Date() + "\n" )
