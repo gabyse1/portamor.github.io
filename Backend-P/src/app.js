0f3dac928a8e9aa3b6cccc08f9d74c3cdc18b9e1
Gaby <gaby.es.sd@gmail.com>
2023-08-22 19:43:05 -0500

    Comment cors

--- Backend-P/src/app.js
+++ Backend-P/src/app.js
@@ -25,16 +25,16 @@ server.use(express.text());
 server.use(cookieParser());
 
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, ContentType, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-  });
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, ContentType, Content-Type, Accept"
+//     );
+//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//     next();
+//   });
   
   server.use("/api", router);
 
